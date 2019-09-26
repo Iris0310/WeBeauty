325fc1bb3f1ae7f1782abdfe842788b36ae6a9ed
iris <irispeng0325@gmail.com>
2019-09-26 05:22:54 +0000

    update url

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index")
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-seedDB();
+//seedDB();
 
 app.use(require("express-session")({
 	secret: "Once again",
