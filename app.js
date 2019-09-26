30d419045b65f0be44fb82971be7fbe1b009c17b
iris <irispeng0325@gmail.com>
2019-09-26 05:49:13 +0000

    update url5

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index")
 
-var url = process.env.DBURL || "mongodb://localhost:27017/yelp_camp"
-mongoose.connect(url, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DBURL, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
